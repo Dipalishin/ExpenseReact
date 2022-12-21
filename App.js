c20a2b30770f7d638dfcf95d6590b30cfbf37c0b
dipa <109209362+Dipalishin@users.noreply.github.com>
2022-12-21 21:23:30 +0530

    task 7

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './ExpenceItem/ExpenceItem';
+function App() {
+  return (
+    <div className="App">
+      <header>
+                <ExpenseItem></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
